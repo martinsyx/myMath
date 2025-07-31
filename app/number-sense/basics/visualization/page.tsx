ad4eb7ae2c22491ab5244a41b4173a9db1c86aea
martinsyx <martinsyx@gmail.com>
2025-07-31 23:34:18 +0800

    fix: add new page

--- app/number-sense/basics/visualization/page.tsx
+++ app/number-sense/basics/visualization/page.tsx
@@ -1,1 +1,10 @@
- 
+ 'use client';
+
+export default function VisualizationPage() {
+  return (
+    <div>
+      <h1>Number Visualization</h1>
+      {/* Add your visualization content here */}
+    </div>
+  );
+}
